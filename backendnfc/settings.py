eec62174d45202a85b3eaa09690c7d136e814dbe
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-08 18:07:55 +0530

    tweaks

--- backendnfc/settings.py
+++ backendnfc/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zd^yk@0k=8$l2lfth&tl-@y^=gv2o8pi0*dx_cu+k#1*g!)t!d'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
