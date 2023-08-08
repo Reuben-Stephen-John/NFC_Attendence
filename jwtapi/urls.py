4c571b05e82b2e4c84ac61cf87c381f829655a21
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-08 17:27:35 +0530

    jwt django completed

--- jwtapi/urls.py
+++ jwtapi/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.get_routes,name="get_routes"),
-    path("get/users/all/",views.NFCUserAPIView.as_view(),name = "get_nfc_users"),
-    path("get/meetings/all/",views.MeetingAPIView.as_view(),name = "get_meetings"),
-    path('mark/attendance/',views.mark_attendance,name = "mark_attedance"),
-    path("get/attendances/all/",views.AttendanceAPIView.as_view(),name = "get_attendance")
+    path("users/",views.NFCUserAPIView.as_view(),name = "get_nfc_users"),#can both get and put new users with token
+    path("meetings/",views.MeetingAPIView.as_view(),name = "get_meetings"),
+    path("attendances/",views.AttendanceAPIView.as_view(),name = "get_attendance")
 ]
--- jwtapi/views.py
+++ jwtapi/views.py
@@ -25,36 +25,12 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(['GET'])
 def get_routes(request):
     routes = [
-        '/get/users/all/',
-        '/get/user/<string:serial>/',
-        '/get/meeting/all/',
-        '/get/attendances/all/',
-        '/mark/attendance/',
+        '/users/',
+        '/meeting/',
+        '/attendances/',
     ]
     return Response(routes)
-@api_view(['POST'])
-def mark_attendance(request):
-    data = request.data
-    meeting = Meeting.objects.get(id=data['meeting_id'])
-    user = NFCUser.objects.get(id=data['user_id'])
-    attendance = Attendance.objects.filter(meeting=meeting, user=user).first()
 
-    if attendance:
-        attendance.status = data['status']
-        formatted_date = datetime.strptime(data['date'], '%Y-%m-%d %H:%M:%S')
-        attendance.date = formatted_date
-        attendance.save()
-    else:
-        formatted_date = datetime.strptime(data['date'], '%Y-%m-%d %H:%M:%S')
-        attendance = Attendance.objects.create(
-            meeting=meeting,
-            user=user,
-            status=data['status'],
-            date=formatted_date
-        )
-
-    # Return a status code as response
-    return Response(status=status.HTTP_200_OK)
         
 ## Class Based Views
 class NFCUserAPIView(generics.ListCreateAPIView):
